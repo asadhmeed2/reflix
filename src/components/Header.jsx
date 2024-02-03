6e3a53b6ff259d1ea6d9e8c55447073d842942b7
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-02-03 18:58:00 +0200

    add logo and bg color to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navbar } from "./Navbar";
+import { Logo } from "./Logo";
 
 export const Header = () => {
   return (
-    <>
+    <div className="header">
       <Navbar />
-    </>
+      <Logo />
+    </div>
   );
 };
